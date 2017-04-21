f43867e631bdeca3b7a7c90cee4b00f8ccee902d
Malash <i@malash.me>
2017-04-22 01:31:12 +0800

    add snapshot support

--- src/store.js
+++ src/store.js
@@ -2,8 +2,15 @@ import { getByPath, setByPath } from './utils';
 
 export default class Store {
 
-  constructor(data) {
+  constructor({
+    data,
+    // FIXME: export api for changing this value
+    maxSnapshots = Infinity,
+  } = {}) {
     this.__data = data;
+    this.__maxSnapshots = maxSnapshots;
+    this.__snapshots = [];
+    this.__snapshotIndex = -1;
   }
 
   read(path) {
@@ -12,5 +19,40 @@ export default class Store {
 
   write(path, value) {
     this.__data = setByPath(this.__data, path, value);
+  }
+
+  snapshot() {
+    this.__snapshotIndex += 1;
+    this.__snapshots[this.__snapshotIndex] = this.__data;
+    // override redid snapshots
+    this.__snapshots.length = this.__snapshotIndex + 1;
+    if (this.__snapshots.length > this.maxSnapshots) {
+      this.__snapshots.shift();
+      this.__snapshotIndex -= 1;
+    }
+  }
+
+  canUndo() {
+    return this.__snapshotIndex > 0;
+  }
+
+  undo() {
+    if (!this.canUndo()) {
+      throw new RangeError('no more snapshot available');
+    }
+    this.__snapshotIndex -= 1;
+    this.__data = this.__snapshots[this.__snapshotIndex];
+  }
+
+  canRedo() {
+    return this.__snapshotIndex + 1 < this.__snapshots.length;
+  }
+
+  redo() {
+    if (!this.canRedo()) {
+      throw new RangeError('no more snapshot available');
+    }
+    this.__snapshotIndex += 1;
+    this.__data = this.__snapshots[this.__snapshotIndex];
   }
 }
